dd4a95c1b8f5885b000d4ccd36125a773329ca1f
Crysthan <113610861+Crysthan@users.noreply.github.com>
2023-04-26 12:44:50 -0300

    Update questao4.java

--- codigos java/questao4.java
+++ codigos java/questao4.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class App {
+    public static void main(String[] args) throws Exception {
+        int idade; 
+        int soma=0;
+        for(int i=0;i<10;i++){
+        Scanner leia = new Scanner(System.in);
+        System.out.println("inf idade");
+        idade = leia.nextInt();
+            
+            soma+=idade
+          }
+        System.out.println("media e " +soma/10);
+        
+        
+        
+        
+        
+    }
+}
